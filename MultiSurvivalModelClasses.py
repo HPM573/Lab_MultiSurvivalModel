270c6fe8d66f2cf0ccd5ba352f27acd4b37d241d
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-02-26 12:06:27 -0500

    1

--- MultiSurvivalModelClasses.py
+++ MultiSurvivalModelClasses.py
@@ -19,19 +19,6 @@ class MultiCohort:
     def simulate(self, n_time_steps):
         """ simulates all cohorts """
 
-        for i in range(len(self.ids)):
-
-            # create a cohort
-            cohort = Cohort(id=self.ids[i], pop_size=self.popSizes[i], mortality_prob=self.mortalityProbs[i])
-
-            # simulate the cohort
-            cohort.simulate(n_time_steps=n_time_steps)
-
-            # outcomes from simulating all cohorts
-            self.multiCohortOutcomes.extract_outcomes(simulated_cohort=cohort)
-
-        # calculate the summary statistics of from all cohorts
-        self.multiCohortOutcomes.calculate_summary_stats()
 
 
 class MultiCohortOutcomes:
@@ -40,50 +27,8 @@ class MultiCohortOutcomes:
         self.survivalTimes = []  # two dimensional list of patient survival times from all simulated cohort
         self.meanSurvivalTimes = []  # list of average patient survival time for all simulated cohort
         self.survivalCurves = []  # list of survival curves from all simulated cohorts
-        self.statMeanSurvivalTime = None  # summary statistics of mean survival time
 
     def extract_outcomes(self, simulated_cohort):
         """ extracts outcomes of a simulated cohort
         :param simulated_cohort: a cohort after being simulated"""
 
-        # store all patient survival times from this cohort
-        self.survivalTimes.append(simulated_cohort.cohortOutcomes.survivalTimes)
-
-        # append the survival curve of this cohort
-        self.survivalCurves.append(simulated_cohort.cohortOutcomes.nLivingPatients)
-
-    def calculate_summary_stats(self):
-        """
-        calculate the summary statistics
-        """
-
-        # calculate average patient survival time for all simulated cohorts
-        for obs_set in self.survivalTimes:
-            self.meanSurvivalTimes.append(sum(obs_set)/len(obs_set))
-
-        # summary statistics of mean survival time
-        self.statMeanSurvivalTime = Stat.SummaryStat(name='Mean survival time',
-                                                     data=self.meanSurvivalTimes)
-
-    def get_cohort_CI_mean_survival(self, cohort_index, alpha):
-        """
-        :returns: the confidence interval of the mean survival time for a specified cohort
-        :param cohort_index: integer over [0, 1, ...] corresponding to the 1st, 2nd, ... simulated cohort
-        :param alpha: significance level
-        """
-
-        stat = Stat.SummaryStat(name='Summary statistics',
-                                data=self.survivalTimes[cohort_index])
-
-        return stat.get_t_CI(alpha=alpha)
-
-    def get_cohort_PI_survival(self, cohort_index, alpha):
-        """ :returns: the prediction interval of the survival time for a specified cohort
-        :param cohort_index: integer over [0, 1, ...] corresponding to the 1st, 2ndm ... simulated cohort
-        :param alpha: significance level
-        """
-
-        stat = Stat.SummaryStat(name='Summary statistics',
-                                data=self.survivalTimes[cohort_index])
-
-        return stat.get_PI(alpha=alpha)

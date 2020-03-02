210dc5fb37b299573a52ed831f67178e2229ed65
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-03-02 09:50:06 -0500

    p0

--- RunMultiSurvivalModel.py
+++ RunMultiSurvivalModel.py
@@ -1,6 +1,6 @@
 import MultiSurvivalModelClasses as Cls
-import SimPy.FigureSupport as Fig
-import SimPy.SamplePathClasses as Path
+import SimPy.Plots.SamplePaths as Path
+import SimPy.Plots.Histogram as Hist
 
 MORTALITY_PROB = 0.1    # annual probability of death
 TIME_STEPS = 100        # years
@@ -8,17 +8,11 @@ N_COHORTS = 500         # number of cohorts
 COHORT_POP_SIZE = 1000   # size of each cohort
 
 # create multiple cohorts
-multiCohort = Cls.MultiCohort(
-    ids=range(N_COHORTS),   # [0, 1, 2 ..., N_COHORTS-1]
-    pop_sizes=[COHORT_POP_SIZE] * N_COHORTS,  # [COHORT_POP_SIZE, COHORT_POP_SIZE, ..., COHORT_POP_SIZE]
-    mortality_probs=[MORTALITY_PROB]*N_COHORTS  # [p, p, ....]
-)
 
 # simulate all cohorts
-multiCohort.simulate(TIME_STEPS)
 
 # plot the sample paths
-Path.graph_sample_paths(
+Path.plot_sample_paths(
     sample_paths=multiCohort.multiCohortOutcomes.survivalCurves,
     title='Survival Curves',
     x_label='Time-Step (Year)',
@@ -26,7 +20,7 @@ Path.graph_sample_paths(
     transparency=0.5)
 
 # plot the histogram of average survival time
-Fig.graph_histogram(
+Hist.plot_histogram(
     data=multiCohort.multiCohortOutcomes.meanSurvivalTimes,
     title='Histogram of Mean Survival Time',
     x_label='Mean Survival Time (Year)',
@@ -34,8 +28,10 @@ Fig.graph_histogram(
 
 # print projected mean survival time (years)
 print('Projected mean survival time (years)',
-      multiCohort.multiCohortOutcomes.sumStat_meanSurvivalTime.get_mean())
+
+      )
 
 # print projection interval
 print('95% projection (prediction, percentile, or uncertainty) interval of average survival time (years)',
-      multiCohort.multiCohortOutcomes.sumStat_meanSurvivalTime.get_PI(alpha=0.05))
+
+      )
